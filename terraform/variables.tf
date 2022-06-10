2935901be2585b1d1f059281282e81d05b78fdf5
OleksandrMakarov <alexander.vmv@gmail.com>
2022-06-10 02:57:52 +0200

    add terraform parts

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "Provide region name:"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "Provide AWS access key:"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "Provide AWS secret key:"
+  type        = string
+}
